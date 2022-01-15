7ea9af855e784257e849dfe4810dd870db45b723
Ben Wespiser <92940727+Wespiseb@users.noreply.github.com>
2022-01-15 13:34:15 -0600

    dumb

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,84 +4,21 @@
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.cscore.UsbCamera;
-import edu.wpi.first.cscore.VideoSink;
 
-import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.can.TalonFX;
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-
-import edu.wpi.first.cameraserver.CameraServer;
-import edu.wpi.first.networktables.NetworkTableEntry;
-import edu.wpi.first.networktables.NetworkTableInstance;
-import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.buttons.JoystickButton;
-import edu.wpi.first.wpilibj.motorcontrol.Talon;
-import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
-import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardComponent;
-import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardContainer;
-import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
-import edu.wpi.first.wpilibj.simulation.JoystickSim;
+
+
 
 
 public class Robot extends TimedRobot {
 
-  private static final int TalonSRX_ID = 30;
-  private static final int Can_Spark_ID = 40;
-  private static final int FalconID = 50;
-  private static final int leverOne_ID = 4;
-  private Joystick leverOne;
-  private TalonFX TheOtherOne;
-  private CANSparkMax Spike_Motor;
-  private TalonSRX grey_motor;
-  private UsbCamera camera1;
-  private UsbCamera camera2;
-  private UsbCamera camera3;
-  private NetworkTableEntry cameraSelection;
-  private VideoSink server;
   
+
   
   @Override
-  public void robotInit() {
-    Spike_Motor = new CANSparkMax(Can_Spark_ID, MotorType.kBrushless);
-    TheOtherOne = new TalonFX(FalconID);
-    grey_motor = new TalonSRX(TalonSRX_ID);
-    leverOne = new Joystick(leverOne_ID);
-    camera1 = CameraServer.startAutomaticCapture(0);
-    camera2 = CameraServer.startAutomaticCapture(1);
-    camera3 = CameraServer.startAutomaticCapture(2);
+  public void robotInit() 
+  {
 
-    cameraSelection = NetworkTableInstance.getDefault().getTable("").getEntry("CameraSelection");
-    
-     // Set talon parameters to default values
-      TheOtherOne.configFactoryDefault();
-
-
-    TheOtherOne.setSensorPhase(true);  // correct encoder to motor direction
-     
-    // Tell the talon that he has a quad encoder
-   // TheOtherOne.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder, 0, 30);
-   
-    // Set minimum output (closed loop)  to 0 for now
-    TheOtherOne.configNominalOutputForward(0, 30);
-    TheOtherOne.configNominalOutputReverse(0, 30);
-    
-    // Set maximum forward and backward to full speed
-    TheOtherOne.configPeakOutputForward(1, 30);
-    TheOtherOne.configPeakOutputReverse(-1, 30);
-
-    // Motion magic cruise (max speed) is 100 counts per 100 ms
-		TheOtherOne.configMotionCruiseVelocity(500, 30);
-
-    // Motion magic acceleration is 50 counts
-		TheOtherOne.configMotionAcceleration(100, 30);
-
-		// Zero the sensor once on robot boot up 
-		TheOtherOne.setSelectedSensorPosition(0, 0, 30);
   }
 
   @Override
@@ -92,38 +29,26 @@ public class Robot extends TimedRobot {
 
   @Override
   public void autonomousInit() {
-    Spike_Motor.set(.1);
-    TheOtherOne.set(ControlMode.PercentOutput, .5);
-    grey_motor.set(ControlMode.PercentOutput, .1);
+
   }
 
   @Override
   public void autonomousPeriodic() {
-    SmartDashboard.putNumber("Faclcon Motor Value", TheOtherOne.getSelectedSensorPosition());
   }
 
   @Override
   public void teleopInit() {
-    
 
   }
 
   @Override
   public void teleopPeriodic() {
-    if (leverOne.getRawButtonPressed(2)) {
-      System.out.println("Setting camera 3");
-      Shuffleboard.selectTab("LimeLight");
-  }else if (leverOne.getRawButtonPressed(1) && !(leverOne.getRawButton(2)))  {
-      System.out.println("Setting camera 1");
-      Shuffleboard.selectTab("Back");
-  }else if (leverOne.getRawButtonReleased(1) && !(leverOne.getRawButton(2))) {
-    System.out.println("Setting camera 2");
-    Shuffleboard.selectTab("Front");
-  }
   }
 
   @Override
-  public void disabledInit() {}
+  public void disabledInit() {
+
+  }
 
   @Override
   public void disabledPeriodic() {}
@@ -133,4 +58,6 @@ public class Robot extends TimedRobot {
 
   @Override
   public void testPeriodic() {}
+
+
 }
